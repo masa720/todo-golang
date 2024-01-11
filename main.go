4b283af4a84a53ba49f79379d4a4bfe3366c2094
Masaki <>
2024-01-11 14:38:49 +0900

    [add]TODOテーブル実装

--- main.go
+++ main.go
@@ -1,39 +1,53 @@
 package main
 
 import (
-	"fmt"
 	"todo/app/models"
 )
 
 func main() {
-	// fmt.Println(config.Config.Port)
-	// fmt.Println(config.Config.SQLDriver)
-	// fmt.Println(config.Config.DbName)
-	// fmt.Println(config.Config.LogFile)
-
-	// log.Println("test")
-
-	fmt.Println(models.Db)
+	// fmt.Println(models.Db)
 
 	// u := &models.User{}
-	// u.Name = "test"
-	// u.Email = "test@example.com"
+	// u.Name = "test2"
+	// u.Email = "test2@example.com"
 	// u.Password = "testtest"
 	// fmt.Println(u)
 
 	// u.CreateUser()
 
-	u, _ := models.GetUser(1)
+	// fmt.Println(u)
 
-	fmt.Println(u)
+	// u.Name = "Test2"
+	// u.Email = "test2@example.com"
+	// u.UpdateUser()
+	// u, _ = models.GetUser(1)
+	// fmt.Println(u)
 
-	u.Name = "Test2"
-	u.Email = "test2@example.com"
-	u.UpdateUser()
-	u, _ = models.GetUser(1)
-	fmt.Println(u)
+	// u.DeleteUser()
+	// u, _ = models.GetUser(1)
+	// fmt.Println(u)
 
-	u.DeleteUser()
-	u, _ = models.GetUser(1)
-	fmt.Println(u)
+	// user, _ := models.GetUser(2)
+	// user.CreateTodo("First Todo")
+
+	// t, _ := models.GetTodo(2)
+	// fmt.Println(t)
+
+	// user, _ := models.GetUser(3)
+	// user.CreateTodo("Third Todo")
+
+	// todos, _ := models.GetTodos()
+	// for _, v := range todos {
+	// 	fmt.Println(v)
+	// }
+
+	// user2, _ := models.GetUser(3)
+	// todos, _ := user2.GetTodosByUser()
+	// for _, v := range todos {
+	// 	fmt.Println(v)
+	// }
+
+	t, _ := models.GetTodo(3)
+	t.DeleteTodo()
+
 }
