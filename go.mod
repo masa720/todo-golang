e92091e2e183c0aefac77dce8afd99f94b49eb97
Masaki <>
2024-01-11 13:54:52 +0900

    [add]Userテーブル実装

--- go.mod
+++ go.mod
@@ -2,4 +2,10 @@ module todo
 
 go 1.21.5
 
-require gopkg.in/go-ini/ini.v1 v1.67.0
+require (
+	github.com/google/uuid v1.5.0
+	github.com/mattn/go-sqlite3 v1.14.19
+	gopkg.in/go-ini/ini.v1 v1.67.0
+)
+
+require github.com/stretchr/testify v1.8.4 // indirect
--- main.go
+++ main.go
@@ -2,15 +2,38 @@ package main
 
 import (
 	"fmt"
-	"log"
-	"todo/config"
+	"todo/app/models"
 )
 
 func main() {
-	fmt.Println(config.Config.Port)
-	fmt.Println(config.Config.SQLDriver)
-	fmt.Println(config.Config.DbName)
-	fmt.Println(config.Config.LogFile)
+	// fmt.Println(config.Config.Port)
+	// fmt.Println(config.Config.SQLDriver)
+	// fmt.Println(config.Config.DbName)
+	// fmt.Println(config.Config.LogFile)
 
-	log.Println("test")
+	// log.Println("test")
+
+	fmt.Println(models.Db)
+
+	// u := &models.User{}
+	// u.Name = "test"
+	// u.Email = "test@example.com"
+	// u.Password = "testtest"
+	// fmt.Println(u)
+
+	// u.CreateUser()
+
+	u, _ := models.GetUser(1)
+
+	fmt.Println(u)
+
+	u.Name = "Test2"
+	u.Email = "test2@example.com"
+	u.UpdateUser()
+	u, _ = models.GetUser(1)
+	fmt.Println(u)
+
+	u.DeleteUser()
+	u, _ = models.GetUser(1)
+	fmt.Println(u)
 }
